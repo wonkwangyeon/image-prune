f5e7c090a63e00fd922847ff7ca3cfbbe7881ace
wonkwangyeon <cocoll2000@naver.com>
2024-04-28 15:53:44 +0900

    feat : add image_prune.sh

---
+++ image_prune.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+
+if [ -z $API_URL ] || [ -z $API_TOKEN ] || [ -z $KEY_NAME ] ; then
+	echo "API URL or API_TOKEN or KEY_NAME is not blank"
+	exit 1
+fi
+
+SSH_KEY=$(ls /etc/sshkey)
+
+if [ -z "$SSH_KEY" ] ; then
+	echo "Required Volume Mount : /etc/sshkey"	
+	exit 1
+fi
+
+OS_USER="${OS_USER:=root}"
+CRI_TYPE="${CRI_TYPE:=docker}"
+IMAGE_PRUNE="docker image prune -a"
+CONTROL_PLANE="${CONTROL_PLANE:=true}"
+IP_LIST=""
+
+if [ $CRI_TYPE == "crictl" ] ; then
+	IMAGE_PRUNE="crictl rmi --prune"
+fi
+
+if [ $CONTROL_PLANE == "true" ] ; then
+	echo "CONTROL_PLANE : true"
+	
+	IP_LIST=($(curl https://$API_URL:6443/api/v1/nodes --header "Authorization: Bearer $API_TOKEN" --insecure | jq '.items[] | select(.status.conditions[] | select(.type=="Ready" and .status=="True"))' | jq '.status.addresses[] | select(.type=="InternalIP") | .address'))
+
+else
+	echo "CONTROL_PLANE : false"
+	
+	IP_LIST=($(curl https://$API_URL:6443/api/v1/nodes --header "Authorization: Bearer $API_TOKEN" --insecure | jq '.items[] | select(.status.conditions[].type=="Ready" and .status.conditions[].status=="True" and .metadata.labels."node-role.kubernetes.io/control-plane"!="")' | jq '.status.addresses[] | select(.type=="InternalIP") | .address'))
+fi
+
+
+for IP in "${IP_LIST[@]}"; do
+	IP=${IP#\"}
+	IP=${IP%\"}
+	echo "CRI_TYPE : $CRI_TYPE"
+        echo "IP : $IP"
+
+	echo "OS_USER : $OS_USER"
+	ssh -i /etc/sshkey/$KEY_NAME -o StrictHostKeyChecking=no $OS_USER@$IP $IMAGE_PRUNE
+done       
+
+echo "done"
